6302ac786bdfdf8301e3ef416773db897117959c
BrayanGuerreroXD <bg7047653@gmail.com>
2022-07-04 16:22:01 -0500

    Se crearon las clases test y secondtest

---
+++ Second Exercise/src/SecondTest.java
@@ -0,0 +1,9 @@
+public class SecondTest {
+    public static void main(String[] args) {
+        System.out.println("no xd");
+    }
+
+    private static void main() {
+        System.out.println("no xd x2");
+    }
+}
---
+++ Second Exercise/src/Test.java
@@ -0,0 +1,20 @@
+/**
+ *Class that tests the program's functionalities
+ *
+ * @version 1.0.0 2022-07-04
+ *
+ * @author Brayan Stewart Guerrero Ordoñez – bg7047653@gmail.com
+ *
+ * @since 1.0.0 2022-07-04
+ *
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("XD");
+    }
+
+    private static void main() {
+        System.out.println("xd x2");
+    }
+}
