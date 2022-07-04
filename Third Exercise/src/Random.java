75d350ba2cf1695a01e310126cff119ddb992c9c
BrayanGuerreroXD <bg7047653@gmail.com>
2022-07-03 21:42:16 -0500

    Se realizaron los metodos necesarios para la solucion al tercer ejercicio

--- Third Exercise/src/Random.java
+++ Third Exercise/src/Random.java
@@ -1,31 +1,35 @@
 public class Random {
     private final int[] randomArray = new int[10];
 
+    public int[] getRandomArray() {
+        return randomArray;
+    }
+
     public void printArray() {
         for(int value: randomArray) {
             System.out.println("" + value);
         }
     }
 
-    private void fillArray() {
+    public void fillArray() {
         for (int x=0;x<randomArray.length;x++)
             randomArray[x] = (int) (Math.random()*20)+1;
     }
 
-    private static void bubble(int[] array) {
-        for (int x = 0; x < array.length; x++) {
-            for (int y = 0; y < array.length - 1; y++) {
-                int current = array[y],
-                        next = array[y + 1];
+    public void bubble() {
+        for (int x = 0; x < this.randomArray.length; x++) {
+            for (int y = 0; y < this.randomArray.length - 1; y++) {
+                int current = this.randomArray[y],
+                        next = this.randomArray[y + 1];
                 if (current > next) {
-                    array[y] = next;
-                    array[y + 1] = current;
+                    this.randomArray[y] = next;
+                    this.randomArray[y + 1] = current;
                 }
             }
         }
     }
 
-    private static void quicksort(int[] array, int izquierda, int derecha) {
+    public void quicksort(int[] array, int izquierda, int derecha) {
         if (izquierda < derecha) {
             int indiceParticion = particion(array, izquierda, derecha);
             quicksort(array, izquierda, indiceParticion);
--- Third Exercise/src/Test.java
+++ Third Exercise/src/Test.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Test {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner in = new Scanner(System.in);
+
+        random.fillArray();
+        random.printArray();
+
+        System.out.println("Con que método deseas ordenar el array? :");
+        System.out.println("1. Burbuja");
+        System.out.println("2. QuickSort");
+
+        int choise = in.nextInt();
+
+        if(choise==1) {
+            random.bubble();
+        }else {
+            random.quicksort(random.getRandomArray(), 0, (random.getRandomArray().length-1));
+        }
+
+        System.out.println("");
+        System.out.println("Array Ordenado: ");
+        random.printArray();
+    }
 }
